08d0a61952137aad20bb922ab14fb9aace11471b
Nghtfury <chrismon1997@gmail.com>
2022-07-10 02:12:10 -0700

    Update in sidemenu

--- src/Components/SideMenu/SideMenu.js
+++ src/Components/SideMenu/SideMenu.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './SideMenu.css'
-import { LineStyle, MonetizationOn, PermIdentity, Timeline, TrendingUp } from '@material-ui/icons';
+import { LineStyle, MonetizationOn, PermIdentity, Timeline,
+     TrendingUp,AccessTime, MoneyTwoTone, AssignmentTwoTone,
+     SupervisorAccountTwoTone, AdbTwoTone } from '@material-ui/icons';
 import { Link } from 'react-router-dom'
 
 function SideMenu() {
@@ -14,14 +16,14 @@ function SideMenu() {
                         <Link to="/" className='link'>
                             <li className='sidebarListItems active'><LineStyle className='sidebarIcon' />Home</li>
                         </Link>
-                        <Link to="/stats" className='link'>
-                            <li className='sidebarListItems'><LineStyle className='sidebarIcon' />Stats</li>
-                        </Link>
+                        {/* <Link to="/stats" className='link'>
+                            <li className='sidebarListItems'><MonetizationOn className='sidebarIcon' />Stats</li>
+                        </Link> */}
                         <Link to="/betting" className='link'>
-                            <li className='sidebarListItems'><LineStyle className='sidebarIcon' />Betting</li>
+                            <li className='sidebarListItems'><AdbTwoTone className='sidebarIcon' />Betting Info</li>
                         </Link>
-                        <Link to="/result" className='link'>
-                            <li className='sidebarListItems'><LineStyle className='sidebarIcon' />Result</li>
+                        <Link to="/resultOnly" className='link'>
+                            <li className='sidebarListItems'><AssignmentTwoTone className='sidebarIcon' />Result</li>
                         </Link>
                         {/* <Link to="/analytics" className='link'>
                             <li className='sidebarListItems'><Timeline className='sidebarIcon' />Analytics</li>
@@ -29,44 +31,48 @@ function SideMenu() {
                         {/* <Link to="/overview" className='link'>
                             <li className='sidebarListItems'><MonetizationOn className='sidebarIcon' />Overview</li>
                         </Link> */}
-                        <Link to="/withdraw" className='link'>
-                            <li className='sidebarListItems'><Timeline className='sidebarIcon' />Withdrow Request</li>
-                        </Link>
+                        
                         {/* <li className='sidebarListItems'><TrendingUp className='sidebarIcon' />Feedback</li> */}
                     </ul>
                 </div>
                 {/* QuickMenu */}
-                {/* <div className='sidebarMenu'>
+                <div className='sidebarMenu'>
                     <h3 className='sidebarTitle'>QuickMenu</h3>
                     <ul className='sidebarList'>
+                        
+                        <Link to="/result" className='link'>
                         <li className='sidebarListItems'><Timeline className='sidebarIcon' />Shedule Bet</li>
+                        </Link>
                         <Link to="/users" className='link'>
                             <li className='sidebarListItems'><PermIdentity className='sidebarIcon' />Users</li>
                         </Link>
-                        <li className='sidebarListItems'><Timeline className='sidebarIcon' />Product</li>
-                        <li className='sidebarListItems'><TrendingUp className='sidebarIcon' />Transations</li>
-                        <li className='sidebarListItems'><TrendingUp className='sidebarIcon' />Report</li>
+                        <Link to="/slider" className='link'>
+                            <li className='sidebarListItems'><AccessTime className='sidebarIcon' />Run Spell</li>
+                        </Link>
                     </ul>
-                </div> */}
+                </div>
                 {/* NOTIFICATIONS */}
-                {/* <div className='sidebarMenu'>
+                <div className='sidebarMenu'>
                     <h3 className='sidebarTitle'>Notifications</h3>
                     <ul className='sidebarList'>
-                        <li className='sidebarListItems'><LineStyle className='sidebarIcon' />Mail</li>
+                    <Link to="/withdraw" className='link'>
+                            <li className='sidebarListItems'><MoneyTwoTone className='sidebarIcon' />Withdrow Request</li>
+                        </Link>
+                        {/* <li className='sidebarListItems'><LineStyle className='sidebarIcon' />Mail</li>
                         <li className='sidebarListItems'><Timeline className='sidebarIcon' />Withdrow Request</li>
                         <li className='sidebarListItems'><TrendingUp className='sidebarIcon' />Messages</li>
-                        <li className='sidebarListItems'><TrendingUp className='sidebarIcon' />Feedback</li>
+                        <li className='sidebarListItems'><TrendingUp className='sidebarIcon' />Feedback</li> */}
                     </ul>
-                </div> */}
+                </div>
                 {/* STAFF */}
                 <div className='sidebarMenu'>
                     <h3 className='sidebarTitle'>Staff</h3>
                     <ul className='sidebarList'>
                         <Link to='/addAdmin' className='link'>
-                            <li className='sidebarListItems'><LineStyle className='sidebarIcon' />Manage Admin</li>
+                            <li className='sidebarListItems'><SupervisorAccountTwoTone className='sidebarIcon' />Manage Admin</li>
                         </Link>
                         <Link to="/newUser" className='link'>
-                            <li className='sidebarListItems'><TrendingUp className='sidebarIcon' />Manage User</li>
+                            <li className='sidebarListItems'><PermIdentity className='sidebarIcon' />Manage User</li>
                         </Link>
                     </ul>
                 </div>
